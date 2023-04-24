e0da9a1b84f31570a90e9a8fde0911d8cf35738e
ms471841 <ms471841@gmail.com>
2023-04-25 01:22:18 +0530

    update

---
+++ eleappbar/android/app/src/main/kotlin/com/example/eleappbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eleappbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ eleappbar/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RunMyApp());
+}
+
+class RunMyApp extends StatelessWidget {
+  const RunMyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Elevation'),
+          elevation: 20,
+        ),
+        body: Center(
+          child: Text(' Elevation 20'),
+        ),
+      ),
+    );
+  }
+}
